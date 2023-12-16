45f5e141d09b5f6ccbe0b169857bf6d9ce049383
darrwem <flymen228@mail.ru>
2023-12-16 11:06:05 +0500

    Осталось доделать локализацию фрагмента на исходнике (Нарисовать рамку по координатам)

--- lab5/main.py
+++ lab5/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from PIL import Image
 from scipy.fft import fft2, ifft2
@@ -12,26 +13,9 @@ def get_image_sizes(image: Image) -> (int, int):
     
     return image.size
 
-def input_fragment_size(width: int, height: int) -> (int, int):
-    
-    input_string = input('Введите ширину и высоту фрагмента изображения через пробел: ').split()
-    
-    return (int(input_string[0]) if width >= int(input_string[0]) else width, 
-            int(input_string[1]) if height >= int(input_string[1]) else height)
-
 def normalize_image(image: Image) -> list:
     
     return np.array(image) / 255
-
-def extract_fragment(image: list, fragment_width: int, fragment_height: int) -> list:
-    
-    center_mass = np.unravel_index(np.argmax(image), image.shape)
-    i, j = center_mass
-    
-    i_start, i_end = max(0, i - fragment_height // 2), min(image.shape[0], i + fragment_height // 2)
-    j_start, j_end = max(0, j - fragment_width // 2), min(image.shape[1], j + fragment_width // 2)
-    
-    return image[i_start:i_end, j_start:j_end]
 
 def normalized_arr_to_image(image_arr: list) -> Image:
     
@@ -46,8 +30,8 @@ def extend_images(image_arr: list, fragment_arr: list, height: int, width: int, 
     center_mass = np.unravel_index(np.argmax(image_arr), image_arr.shape)
     i, j = center_mass
 
-    i_start, i_end = max(0, i - fragment_height // 2), min(height, i + fragment_height // 2)
-    j_start, j_end = max(0, j - fragment_width // 2), min(width, j + fragment_width // 2)
+    i_start, i_end = max(0, i - fragment_width // 2), min(width, i + fragment_width // 2)
+    j_start, j_end = max(0, j - fragment_height // 2), min(height, j + fragment_height // 2)
     
     temp_arr[i_start:i_end, j_start:j_end] = fragment_arr
 
@@ -68,27 +52,52 @@ def linear_correlation(image_arr: list, fragment_arr: list) -> list:
     
     Fu_x_Fv_conj = Fu * Fv_conj
     
-    image_x_fragment = ifft2(Fu_x_Fv_conj)
+    width, height = image_arr.shape
+    fragment_width, fragment_height = fragment_arr.shape
     
-    return np.abs(image_x_fragment)
-
-def inverse_linear_correlation(linear_correlation_arr: list) -> list:
+    P, R = np.meshgrid(np.fft.fftfreq(height), np.fft.fftfreq(width))
     
-    return np.abs(ifft2(linear_correlation_arr))
-
-def reduce_matrix(arr: list, height: int, width: int, fragment_height: int, fragment_width: int) -> list:
+    exp = np.exp(-2j * np.pi * (P * (fragment_width - 1) / width + R * (fragment_height - 1) / height))
     
-    center_mass = np.unravel_index(np.argmax(arr), arr.shape)
-    i, j = center_mass
+    return np.abs(ifft2(exp * Fu_x_Fv_conj))
 
-    i_start, i_end = max(0, i - height // 2), min(fragment_height, i + height // 2)
-    j_start, j_end = max(0, j - width // 2), min(fragment_width, j + width // 2)
-
-    return arr[i_start : i_end, j_start : j_end]
-
-def find_max_elem(arr: list) -> int:
+def reduce_matrix(linear_cor_arr: list, width: int, height: int, fragment_width: int, fragment_height: int) -> (int, int, int, int):
     
-    return np.max(arr)
+    max_index = np.unravel_index(np.argmax(linear_cor_arr), linear_cor_arr.shape)
+    
+    i_start = max(0, max_index[0] - fragment_width // 2)
+    i_end = min(width, max_index[0] + fragment_width // 2 + 1)
+
+    j_start = max(0, max_index[1] - fragment_height // 2)
+    j_end = min(height, max_index[1] + fragment_height // 2 + 1)
+    
+    return (i_start, i_end, j_start, j_end)
+
+def locolize_fragment(image: Image, cords : tuple) -> Image:
+    
+    
+
+def plot_3_images(image1: Image, image2: Image, image3: Image) -> None:
+    
+    plt.figure(figsize=(12, 4))
+
+    plt.subplot(1, 3, 1)
+    plt.title("Original Gray Image")
+    plt.imshow(image1, cmap="gray")
+    plt.axis('off')
+
+    plt.subplot(1, 3, 2)
+    plt.title("Fragment Gray")
+    plt.imshow(image2, cmap="gray")
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.title("Reduced Fragment")
+    plt.imshow(image3, cmap="gray")
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig('output_image.png')
 
 
 def main():
@@ -101,13 +110,15 @@ def main():
     
     width, height = get_image_sizes(gray_image)
     
-    fragment_width, fragment_height = input_fragment_size(width, height)
-    
     gray_image_array = normalize_image(gray_image)
     
-    fragment_arr = extract_fragment(gray_image_array, fragment_width, fragment_height)
+    fragment = image_to_gray(Image.open('fragment.jpg'))
     
-    normalized_arr_to_image(fragment_arr).save('fragment.jpg')
+    fragment.save('gray_fragment.jpg')
+    
+    fragment_arr = normalize_image(fragment)
+    
+    fragment_height, fragment_width = get_image_sizes(fragment)
     
     extended_gray_image_arr, extended_fragment_arr = extend_images(gray_image_array, fragment_arr, height, width, fragment_height, fragment_width)
     
@@ -117,17 +128,12 @@ def main():
     
     linear_correlation_arr = linear_correlation(extended_gray_image_arr, extended_fragment_arr)
     normalized_arr_to_image(linear_correlation_arr).save('linear_correlation.jpg')
-    
-    inverse_linear_correlation_arr = inverse_linear_correlation(linear_correlation_arr)
-    normalized_arr_to_image(inverse_linear_correlation_arr).save('inverse_linear_correlation.jpg')
 
-    reduced_matrix = reduce_matrix(inverse_linear_correlation_arr, height, width, fragment_height, fragment_width)
-    normalized_arr_to_image(reduced_matrix).save('reduced_image.jpg')
+    reduced_cords = reduce_matrix(linear_correlation_arr, width, height, fragment_width, fragment_height)
     
-    max_elem = find_max_elem(reduced_matrix)
+    locolized_fragment_image = locolize_fragment(gray_image, reduced_cords)
     
-    print(f'Максимальный элемент в редуцированной матрице: {max_elem}')
-    
+    plot_3_images(gray_image, fragment, reduced_image)
 
 if __name__ == '__main__':
     main()
